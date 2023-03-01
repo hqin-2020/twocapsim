eb45173e01bc0b233f95f44c86c536c76e3dae80
hqin-2020 <uctphqi@ucl.ac.uk>
2023-03-01 11:04:05 -0600

    new commit

--- plot_rho_3d.sh
+++ plot_rho_3d.sh
@@ -19,7 +19,7 @@ for Delta in ${Deltaarray[@]}; do
             for gamma in "${gammaarray[@]}"; do
                     count=0
 
-                    action_name="TwoCapital_julia_rhoeq_required_test_more_grid4"
+                    action_name="TwoCapital_julia_rhoeq_required_test_more_grid5"
                     # action_name="TwoCapital_julia_rhoeq_time_51754"
 
                     dataname="${action_name}_${Delta}_frac_${fraction}"
