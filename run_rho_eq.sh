4ec752fd13b16a05eaaf5b13c047c4e6a2992bdc
hqin-2020 <uctphqi@ucl.ac.uk>
2023-03-01 13:50:16 -0600

    new commit

--- run_rho_eq.sh
+++ run_rho_eq.sh
@@ -3,18 +3,20 @@
 # Deltaarray=(5 10 20 30 50 100 150 500 1000 5000 10000)
 # fractionarray=(0.1 0.05 0.01 0.005 0.001 0.0005 0.0001)
 Deltaarray=(5 10 20 50 100 150 300)
+Deltaarray=(50 100 150)
 fractionarray=(0.0)
 
 actiontime=1
 
-julia_name="newsets_twocapitals_rhoas.jl"
+julia_name="newsets_twocapitals_rhoeq.jl"
 
 # rhoarray=(0.7 0.8 0.9 1.00001 1.1 1.2 1.3 1.4 1.5)
 rhoarray=(1.00001)
-gammaarray=(1.1 2.0 4.0 8.0)
+# gammaarray=(1.1 2.0 4.0 8.0)
 # gammaarray=(1.00001 2.0 4.0 8.0)
 # gammaarray=(1.01 1.1 1.25 1.5 2.0 4.0 8.0)
 # gammaarray=(4.1 4.3 4.5 5.0 6.0)
+gammaarray=(4.6 4.7 4.8 4.9)
 
 for Delta in ${Deltaarray[@]}; do
     for fraction in "${fractionarray[@]}"; do
@@ -23,8 +25,8 @@ for Delta in ${Deltaarray[@]}; do
                     count=0
 
                     # action_name="TwoCapital_julia_rhoeq_more_test"
-                    # action_name="TwoCapital_julia_rhoeq_more_grids_gamma_56"
-                    action_name="TwoCapital_julia_rhoeq_standard_grids_as"
+                    action_name="TwoCapital_julia_rhoeq_more_grids_gamma_56"
+                    # action_name="TwoCapital_julia_rhoeq_standard_grids_as"
 
                     dataname="${action_name}_${Delta}_frac_${fraction}"
 
