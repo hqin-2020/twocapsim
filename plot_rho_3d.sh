925cd0fb221fc5548bd062ee5fc47061296fc630
hqin-2020 <uctphqi@ucl.ac.uk>
2023-03-01 13:39:44 -0600

    new commit

--- plot_rho_3d.sh
+++ plot_rho_3d.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # Deltaarray=(100 500 1000)
-Deltaarray=(50 100 150 300 500)
+Deltaarray=(50 100 150 300)
 fractionarray=(0.0)
 
 actiontime=1
@@ -11,7 +11,8 @@ python_name="plots_rho_moregrid.py"
 rhoarray=(0.7 0.8 0.9 1.00001 1.1 1.2 1.3 1.4 1.5)
 rhoarray=(1.00001)
 # gammaarray=(8.0)
-gammaarray=(1.01 1.1 1.25 1.5 2.0 4.0 8.0)
+# gammaarray=(1.01 1.1 1.25 1.5 2.0 4.0 8.0)
+gammaarray=(4.1 4.3 4.5 5.0 6.0)
 
 for Delta in ${Deltaarray[@]}; do
     for fraction in "${fractionarray[@]}"; do
